bfa88d2b8c360019b59f816a04e5eb321617142b
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-04-29 14:10:22 +0800

    fixing dependencies

--- resources/views/users/forbidden.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.user')
-
-@section('content')
-<h1>Bawal</h1>
-<a href="{{route('users.login')}}">You must Login first</a>
--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-
-@extends('layout.app')
-
-@section('content')
-<a href="{{route('users.login')}}">Login</a>
